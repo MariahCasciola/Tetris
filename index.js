b5795b5176d9715c4a9983a95527920a3398d3fb
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-06-06 21:59:06 -0500

    playing with positions of cells based on the grid created with each cell, removed canvas styling

--- index.js
+++ index.js
@@ -2,76 +2,88 @@
 const canvas = document.querySelector("#canvas");
 const context = canvas.getContext("2d");
 
-// const canvasWidth = canvas.width;
-// const canvasHeight = canvas.height;
+const displayWidth = canvas.width;
+const displayHeight = canvas.height;
 
-// const gridWidth = 24;
-// const gridHeight = 24;
+// grid should be 10 by 20, tetris is 10 by 20
+// or 10 x 24 
+const gridWidth = 10;
+const gridHeight = 20;
 
-// const cellWidth = canvasWidth / gridWidth;
-// const cellHeight = canvasHeight / gridWidth;
-const cellWidth = 20;
-const cellHeight = 20;
+const cellWidth = displayWidth / gridWidth;
+const cellHeight = displayHeight / gridHeight;
 
-// tetris is 10 cells wide
+// x, y, width, height is the fillRect parameters
+// canvas is black
+function main() {
+  context.fillStyle = "black";
+  context.fillRect(0, 0, displayWidth, displayHeight);
+}
+main();
+
+// tetris is 10 cells wide and 20 cells tall
 function drawCell(x, y) {
   context.fillStyle = "white";
-  context.fillRect(x, y, cellWidth, cellHeight);
+  context.fillRect(x, y, cellWidth, cellHeight)
 }
 
-function renderOrangeRicky() {
-  drawCell(50, 20);
-  drawCell(50, 41);
-  drawCell(50, 62);
-  drawCell(71, 62);
-}
+drawCell(1,1)
+drawCell(cellWidth, cellHeight);
+drawCell(cellWidth*2, cellHeight*2);
 
-function renderBlueRickey() {
-  drawCell(100, 20);
-  drawCell(121, 20);
-  drawCell(121, 41);
-  drawCell(121, 62);
-}
+// function renderOrangeRicky() {
+//   drawCell(50, 20);
+//   drawCell(50, 41);
+//   drawCell(50, 62);
+//   drawCell(71, 62);
+// }
 
-function smashBoy() {
-  drawCell(160, 20);
-  drawCell(160, 41);
-  drawCell(181, 41);
-  drawCell(181, 20);
-}
+// function renderBlueRickey() {
+//   drawCell(100, 20);
+//   drawCell(121, 20);
+//   drawCell(121, 41);
+//   drawCell(121, 62);
+// }
 
-function clevelandZ() {
-  drawCell(220, 20);
-  drawCell(241, 41);
-  drawCell(241, 20);
-  drawCell(262, 41);
-}
+// function smashBoy() {
+//   drawCell(160, 20);
+//   drawCell(160, 41);
+//   drawCell(181, 41);
+//   drawCell(181, 20);
+// }
 
-function rhodeIslandZ() {
-  drawCell(290, 41);
-  drawCell(311, 41);
-  drawCell(311, 20);
-  drawCell(332, 20);
-}
+// function clevelandZ() {
+//   drawCell(220, 20);
+//   drawCell(241, 41);
+//   drawCell(241, 20);
+//   drawCell(262, 41);
+// }
 
-function teeWee() {
-  drawCell(50, 121);
-  drawCell(71, 121);
-  drawCell(71, 100);
-  drawCell(92, 121)
-}
+// function rhodeIslandZ() {
+//   drawCell(290, 41);
+//   drawCell(311, 41);
+//   drawCell(311, 20);
+//   drawCell(332, 20);
+// }
 
-function hero() {
-  drawCell(130, 100);
-  drawCell(151, 100);
-  drawCell(172, 100);
-  drawCell(193, 100);
-}
+// function teeWee() {
+//   drawCell(50, 121);
+//   drawCell(71, 121);
+//   drawCell(71, 100);
+//   drawCell(92, 121);
+// }
 
-renderOrangeRicky();
-renderBlueRickey();
-smashBoy();
-teeWee();
-clevelandZ();
-rhodeIslandZ();
-hero();
+// function hero() {
+//   drawCell(130, 100);
+//   drawCell(151, 100);
+//   drawCell(172, 100);
+//   drawCell(193, 100);
+// }
+
+// renderOrangeRicky();
+// renderBlueRickey();
+// smashBoy();
+// teeWee();
+// clevelandZ();
+// rhodeIslandZ();
+// hero();
